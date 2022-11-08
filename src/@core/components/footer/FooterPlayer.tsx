ce0a7103d6c9cb645249fb8ec5b4e8247534146a
nguyendinhphongdx <phongnguyendx@gmail.com>
2022-11-08 19:00:28 +0700

    media control

--- src/@core/components/footer/FooterPlayer.tsx
+++ src/@core/components/footer/FooterPlayer.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Icon } from '@iconify/react';
 import Lottie from 'react-lottie';
 import animationData from '../../../../public/lottie/play-pause.json';
@@ -26,9 +26,7 @@ const FooterPlayer: FC<Props> = (props: Props) => {
     return (
         <div className={styles.container}>
             <SongContainer song={songSelected} />
-            <div className={styles.songPlayer}>
-                song player
-            </div>
+            <MediaPlayer song={songSelected} />
             <ControlMedia song={songSelected} />
         </div>
     );
@@ -68,26 +66,57 @@ const ControlMedia: FC<ControlMediaProps> = (props: ControlMediaProps) => {
         autoplay: false,
         animationData: animationData,
         rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice"
+            preserveAspectRatio: "xMidYMid slice"
         }
-      };
+    };
     return (
         <div className={styles.songContainer}>
-            <div className={styles.image}>
-                <Image src={song.image} alt={song.image} width={60} height={60} />
-            </div>
-            <div className={styles.mation}>
-                <p className={styles.name}>{props.song.name}</p>
-                <span className={styles.singer}>{props.song.singer}</span>
-            </div>
             <div className={styles.actions}>
-                <Icon icon="ant-design:heart-filled" />
-                <Icon icon="bi:three-dots" />
-                <Lottie
-                    options={defaultOptions}
-                    height={400}
-                    width={400}
-                />
+                <Icon icon="ant-design:youtube-outlined" height={20} />
+                <Icon icon="maki:karaoke" height={20} />
+                <Icon icon="carbon:shrink-screen" height={20} />
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 10 }}>
+                    <Icon icon="clarity:volume-up-solid" height={20} />
+                    <input type="range" id="volume-slider" max="100" value="50
+                    " />
+                </div>
+            </div>
+
+        </div>
+    );
+};
+interface MediaPlayerProps {
+    song: SongItem;
+}
+
+const MediaPlayer: FC<MediaPlayerProps> = (props: MediaPlayerProps) => {
+    const { song } = props;
+    const [state, setState] = useState({
+        playing: false
+    });
+    const handleChangeState = (object: any) => {
+        setState({
+            ...state,
+            ...object,
+        });
+    }
+    return (
+        <div className={styles.mediaPlayer}>
+            <div className={styles.actions}>
+                <Icon icon="el:random" color="white" />
+                <Icon icon="bx:skip-previous" color="white" height={30} />
+                {
+                    state.playing
+                        ? <Icon icon="bx:pause-circle" height={30} onClick={() => handleChangeState({ playing: false })} />
+                        : <Icon icon="ant-design:play-circle-outlined" height={30} onClick={() => handleChangeState({ playing: true })} />
+                }
+                <Icon icon="bx:skip-next" color="white" height={30} />
+                <Icon icon="cil:loop" color="white" />
+            </div>
+            <div className={styles.slideSeek}>
+                <span className={styles.duration}>00:00</span>
+                <input type="range" id="volume-slider" max="100" value="50" style={{ maxWidth: '60%', width: '100%' }} />
+                <span className={styles.duration}>00:00</span>
             </div>
         </div>
     );
